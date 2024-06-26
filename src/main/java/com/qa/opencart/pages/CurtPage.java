53d06379dd6b0e9a93a5822b4891d9be253a9e57
email2rakeshsingh <email2rakeshsingh@gmail.com>
2024-06-26 11:57:20 +0530

    added cart page

---
+++ src/main/java/com/qa/opencart/pages/CurtPage.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.pages;
+
+public class CurtPage {
+
+	public static void main(String[] args) {
+		int a = 20;
+		int b = 10;
+		int c = a + b;
+		System.out.println(c);
+		System.out.println("product c added in cart page ");
+
+	}
+
+}
